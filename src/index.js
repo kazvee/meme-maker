795795f3d78cfc47277352b72c8e191d32132438
kazvee <kazveedev@gmail.com>
2024-03-15 11:55:20 -0600

    feat: implement thunk middleware for async actions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
-import ReactDOM from 'react-dom/client';
+import { configureStore } from '@reduxjs/toolkit';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { thunk } from 'redux-thunk';
+import { fetchMemes } from './actions';
 import App from './components/App';
+import rootReducer from './reducers';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+});
+
+store.subscribe(() => console.log('store', store.getState()));
+store.dispatch(fetchMemes());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
