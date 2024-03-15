c2c30437fa84bd34e6fcfd766d0575faa83835c0
kazvee <kazveedev@gmail.com>
2024-03-15 08:22:56 -0600

    chore: initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Meme Maker</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
