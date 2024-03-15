58d286f28896484a9039c7f7ae57de04ed1debe5
kazvee <kazveedev@gmail.com>
2024-03-15 17:41:14 -0600

    feat: display meme names on page

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import { Component } from 'react';
+import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Meme Maker</h1>
+        {this.props.memes.map((meme, index) => {
+          return <h2 key={index}>{meme.name}</h2>;
+        })}
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return state;
+}
+
+export default connect(mapStateToProps, null)(App);
