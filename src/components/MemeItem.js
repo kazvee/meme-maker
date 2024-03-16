bfe7f35cc50d21c6eb784ab596d1b47c3a7c2751
kazvee <kazveedev@gmail.com>
2024-03-15 19:48:03 -0600

    style: add animation to MemeItem elements

--- src/components/MemeItem.js
+++ src/components/MemeItem.js
@@ -1,15 +1,34 @@
 import { Component } from 'react';
 
 class MemeItem extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hovered: false,
+    };
+  }
+
   render() {
     return (
-      <div className='meme-item'>
+      <div
+        className='meme-item'
+        onMouseEnter={() => this.setState({ hovered: true })}
+        onMouseLeave={() => this.setState({ hovered: false })}
+      >
         <img
           src={this.props.meme.url}
           alt={this.props.meme.name}
-          className='meme-image'
+          className={
+            this.state.hovered 
+            ? 'meme-image darken-image' 
+            : 'meme-image'
+          }
         />
-        <p className='meme-text'>{this.props.meme.name}</p>
+        <p className={this.state.hovered 
+          ? 'meme-text' 
+          : 'no-text'}>
+          {this.props.meme.name}
+        </p>
       </div>
     );
   }
