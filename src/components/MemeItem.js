beb3646c70153f6926f836033ff192a4bb051ce4
kazvee <kazveedev@gmail.com>
2024-03-16 11:51:30 -0600

    feat: connect MemeItem to Redux for posting memes

--- src/components/MemeItem.js
+++ src/components/MemeItem.js
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+import { connect } from 'react-redux';
+import { newMeme } from '../actions';
 
 class MemeItem extends Component {
   constructor() {
@@ -10,6 +12,13 @@ class MemeItem extends Component {
 
   postMeme() {
     console.log('this.props', this.props);
+    const { text0, text1 } = this.props;
+    const memeObj = {
+      template_id: this.props.meme.id,
+      text0,
+      text1,
+    };
+    this.props.newMeme(memeObj);
   }
 
   render() {
@@ -35,4 +44,4 @@ class MemeItem extends Component {
   }
 }
 
-export default MemeItem;
+export default connect(null, { newMeme })(MemeItem);
