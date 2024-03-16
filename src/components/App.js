31349254d1a7d20c836dc6be6d7b58c02124c31c
kazvee <kazveedev@gmail.com>
2024-03-15 18:17:32 -0600

    feat: add memeLimit to only load 10 at a time

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,28 @@ import { Component } from 'react';
 import { connect } from 'react-redux';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      memeLimit: 10,
+    };
+  }
+
   render() {
     return (
       <div>
         <h1>Meme Maker</h1>
-        {this.props.memes.map((meme, index) => {
+        {this.props.memes.slice(0, this.state.memeLimit).map((meme, index) => {
           return <h2 key={index}>{meme.name}</h2>;
         })}
+        <div
+          onClick={() => {
+            this.setState({ memeLimit: this.state.memeLimit + 10 });
+          }}
+        >
+          Load 10 More Memes
+        </div>{' '}
       </div>
     );
   }
