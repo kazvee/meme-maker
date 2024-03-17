ca10944851241bd7ab9d5948643d673469c41855
kazvee <kazveedev@gmail.com>
2024-03-17 11:17:10 -0600

    chore: update secrets example file

--- src/actions/secrets.EXAMPLE.js
+++ src/actions/secrets.EXAMPLE.js
@@ -1,9 +1,6 @@
 const API_USERNAME = ''; // Your API username
 const API_PASSWORD = ''; // Your API password
-const API_BASE_URL = ''; // Base URL for the API
-const GET_API_ENDPOINT = ''; // Endpoint for fetching memes
-const CAPTION_API_ENDPOINT = ''; // Endpoint for captioning images
-const GET_API_URL = API_BASE_URL + GET_API_ENDPOINT;
-const CAPTION_API_URL = API_BASE_URL + CAPTION_API_ENDPOINT;
+const GET_API_URL = ''; // URL for fetching memes
+const CAPTION_API_URL = ''; // URL for captioning images
 
 export { API_PASSWORD, API_USERNAME, CAPTION_API_URL, GET_API_URL };
